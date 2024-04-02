980385bf0a3ffa9a9e50522d8787078dbe220cf9
Joseph <202180024@psu.palawan.edu.ph>
2024-04-02 15:26:23 +0800

    models and admin working properly

--- projectsite/studentorg/models.py
+++ projectsite/studentorg/models.py
@@ -10,14 +10,14 @@ class BaseModel(models.Model):
 class College(BaseModel):
     college_name = models.CharField(max_length=150)
 
-    def _str_(self):
+    def __str__(self):
         return self.college_name
     
 class Program(BaseModel):
     prog_name = models.CharField(max_length=150)
     college = models.ForeignKey(College, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return self.prog_name
     
 class Organization(BaseModel):
@@ -26,7 +26,7 @@ class Organization(BaseModel):
         College, null=True, blank=True, on_delete=models.CASCADE)
     description = models.CharField(max_length=500)
 
-    def _str_(self):
+    def __str__(self):
         return self.name
     
 class Student(BaseModel):
@@ -36,7 +36,7 @@ class Student(BaseModel):
     middlename = models.CharField(max_length=25, blank=True, null=True)
     program = models.ForeignKey(Program, on_delete=models.CASCADE)
 
-    def _str_(self):
+    def __str__(self):
         return f"{self.lastname}, {self.firstname}"
     
 class OrgMember(BaseModel):
