94826f85437827bd78439a8acd65cba53a5b9323
Joseph <202180024@psu.palawan.edu.ph>
2024-04-16 10:07:42 +0800

    logout functionality added

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -4,10 +4,14 @@ from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from studentorg.models import Organization, OrgMember, Student, College
 from studentorg.forms import OrganizationForm, OrgMemberForm, StudentForm, CollegeForm
 from django.urls import reverse_lazy
+from django.utils.decorators import method_decorator
+from django.contrib.auth.decorators import login_required
 from typing import Any
 from django.db.models.query import QuerySet
 from django.db.models import Q
 
+
+@method_decorator(login_required, name='dispatch')
 class HomePageView(ListView):
     model = Organization
     context_object_name = 'home'
